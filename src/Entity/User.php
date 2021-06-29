1a2e1c39c51ffff3c06dc9282ad810c50098e807
SergioNodriza <sergio.gomez@nodrizatech.com>
2021-06-29 14:40:12 +0200

    User Register

---
+++ src/Entity/User.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @method string getUserIdentifier()
+ * @ORM\HasLifecycleCallbacks()
+ */
+class User implements UserInterface
+{
+    /*
+    * Timestampable trait
+    */
+    use Timestampable;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private string $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="string", unique=true, length=255)
+     */
+    private string $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $password;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private array $roles = [];
+
+
+    public function __construct(string $name, $email) {
+        $this->id = Uuid::v4()->toRfc4122();
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function addRoles(string $rol): void
+    {
+        if (in_array($rol, $this->roles, true)) {
+            return;
+        }
+        $this->roles[] = $rol;
+    }
+
+    public function getSalt(): void
+    {
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function getUsername(): string
+    {
+        return $this->name;
+    }
+
+    public function __call($name, $arguments)
+    {
+        return $this->getUsername();
+    }
+}
